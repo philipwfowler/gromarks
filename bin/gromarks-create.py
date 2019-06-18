ef78abfd32c0420882e811d8087a65caa5bf6b59
Philip Fowler <philipwfowler@gmail.com>
2019-06-18 10:29:35 +0100

    modified for JADE/SLURM

--- bin/gromarks-create.py
+++ bin/gromarks-create.py
@@ -9,57 +9,97 @@ aparser.add_argument("--id",default=None,help="the name of the JSON describing t
 aparser.add_argument("--protein",default=None,help="the name of the benchmarking TPR file e.g. dhfr/rpob/peptst")
 options = aparser.parse_args()
 
+
 standard_options=" -resethway -maxh 0.1 -noconfout -pin on"
 
 # load the JSON file
 with open("config/machines/"+options.id+".json",'r') as f:
     machine=json.load(f)
 
+assert machine["how-run"] in ["bash","scheduler"], "how-run not set correctly in JSON file, must be one of bash/scheduler"
+
 output_folder=options.protein+"-"+options.id
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-OUTPUT=open(output_folder+"/run.sh",'w')
-
-OUTPUT.write("#! /bin/bash\n")
-OUTPUT.write(""+"\n")
-
 # source the relevant GMXRC
-line = "source "+ machine["gromacs"]["path"]
-OUTPUT.write(line+"\n")
-OUTPUT.write(""+"\n")
+if machine["how-run"]=="bash":
+    OUTPUT=open(output_folder+"/run.sh",'w')
+
+    OUTPUT.write("#! /bin/bash\n")
+    OUTPUT.write(""+"\n")
+
+    line = "source "+ machine["gromacs"]["path"]
+    OUTPUT.write(line+"\n")
+    OUTPUT.write(""+"\n")
+
 
 power_of_2=int(math.log(machine["core-number"])/math.log(2))
 number_cores= [ 2**i for i in range(0,power_of_2+1) ]
 
-number_gpus = range(0,machine["gpu-number"]+1)
+if machine["gpu-number"]>0:
+    number_gpus = range(machine["gpu-number-min"],machine["gpu-number"]+1)
+else:
+    number_gpus=[0]
 
+# iterate over the number of GPUs
 for ngpu in number_gpus:
+
     for ntmpi in number_cores:
-        if (ntmpi*ngpu)<=machine["core-number"]:
 
-            power_of_2=int(math.log(machine["core-number"]/ntmpi)/math.log(2))
-            number_threads=[ 2**a for a in range(0,power_of_2+1) ]
+        power_of_2=int(math.log(machine["core-number"]/ntmpi)/math.log(2))
+        number_threads=[ 2**a for a in range(0,power_of_2+1) ]
 
-            for ntomp in number_threads:
+        for ntomp in number_threads:
+
+            if (ntmpi*ntomp)<=machine["core-number"]:
+
+                stem=options.protein+'_'+options.id+"_"+machine["gromacs"]["version"]+"_"+str(ntmpi)+"_"+str(ngpu)+"_"+str(ntomp)
+
                 line=""
-                tpr_file=None
+
                 if ngpu>0 and ntmpi==ngpu:
-                    # print ngpu, ntmpi, ntomp
-                    tpr_file=options.protein+'_'+options.id+"_"+machine["gromacs"]["version"]+"_"+str(ntmpi)+"_"+str(ngpu)+"_"+str(ntomp)
-                    OUTPUT.write("cp ../config/tpr-files/"+options.protein+".tpr "+tpr_file+".tpr"+"\n")
-                    line = "mpirun -np "+str(ntmpi)+" mdrun_mpi -deffnm "+tpr_file+" -ntomp "+str(ntomp)+standard_options+"\n"
-                    OUTPUT.write(line+"\n")
-                elif ngpu==0:
-                    # print ngpu, ntmpi, ntomp
-                    tpr_file=options.protein+'_'+options.id+"_"+machine["gromacs"]["version"]+"_"+str(ntmpi)+"_"+str(ngpu)+"_"+str(ntomp)
-                    OUTPUT.write("cp ../config/tpr-files/"+options.protein+".tpr "+tpr_file+".tpr"+"\n")
-                    if machine["gpu-number"]>0:
-                        line = "mpirun -np "+str(ntmpi)+" mdrun_mpi -deffnm "+tpr_file+" -ntomp "+str(ntomp)+" -nb cpu"+standard_options+"\n"
+
+                    if machine["how-run"]=="scheduler":
+                        OUTPUT=open(output_folder+"/"+stem+".sh","w")
+                        line=machine["gromacs"]["header"]
+                        line+="\n"
+                        line+=machine["gromacs"]["module-cmds"]
+                        line+="\n"
+                        line+=machine["gromacs"]["command"]
+
                     else:
-                        line = "mpirun -np "+str(ntmpi)+" mdrun_mpi -deffnm "+tpr_file+" -ntomp "+str(ntomp)+standard_options+"\n"
+                        line=""
+
+                elif ngpu==0:
+
+                    if machine["how-run"]=="scheduler":
+                        OUTPUT=open(output_folder+"/"+stem+".sh","w")
+                        line=machine["gromacs"]["header"]
+                        line+="\n"
+                        line+=machine["gromacs"]["module-cmds"]
+                        line+="\n"
+                        line+=machine["gromacs"]["command"]
+                    else:
+                        line+=machine["gromacs"]["command"]
+
+
+                    # print ngpu, ntmpi, ntomp
+
+                    if machine["gpu-number"]>0:
+                        line += " -nb cpu"
+
+                if line!="":
+                    line=line.replace("NTMPI",str(ntmpi))
+                    line=line.replace("NTOMP",str(ntomp))
+                    line=line.replace("TPRFILE",str(stem))
+                    line=line.replace("PROTEIN",options.protein)
+                    print(line)
                     OUTPUT.write(line+"\n")
 
+                if machine["how-run"]=="scheduler" and line !="":
+                    OUTPUT.close()
 
-OUTPUT.close()
+if machine["how-run"]=="bash":
+    OUTPUT.close()
