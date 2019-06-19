02e7955cec35dfc44b72b7feacec923f9b5a7dfb
Philip Fowler <philipwfowler@gmail.com>
2019-06-19 13:16:18 +0100

    swapped JSON files to YAML

--- bin/gromarks-create.py
+++ bin/gromarks-create.py
@@ -1,107 +1,89 @@
 #! /usr/bin/env python
 
-import argparse, pkg_resources
+import argparse, pkg_resources, pathlib, os
 import yaml
-import math, os
+import math
 
 aparser = argparse.ArgumentParser()
-aparser.add_argument("--id",default=None,help="the name of the YAML describing the machine configuration e.g. rescomp.F")
+aparser.add_argument("--machine",default=None,help="the name of the YAML describing the machine configuration e.g. rescomp.F")
 aparser.add_argument("--protein",default=None,help="the path to the benchmarking TPR file e.g. dhfr/rpob/peptst")
 options = aparser.parse_args()
 
+# since we've been given a relative path to the benchmarking TPR, split it and find out the name of the protein
 (tpr_folder,tpr_filename)=os.path.split(options.protein)
-
 protein=tpr_filename.split('.tpr')[0]
 
+
+# now find and then load the YAML file
 resource_package = __name__
-resource_path = '/'.join(('../config/machines/', options.id+".yaml"))  # Do not use os.path.join()
+resource_path = '/'.join(('../config/machines/', options.machine+".yaml"))  # Do not use os.path.join()
 machine_file = pkg_resources.resource_filename(resource_package, resource_path)
-
-# load the YAML file
 with open(machine_file,'r') as f:
-    machine=yaml.load(f)
+    machine_description=yaml.load(f)
 
-assert machine["how-run"] in ["bash","scheduler"], "how-run not set correctly in YAML file, must be one of bash/scheduler"
+# create the output path if it doesn't already exist
+output_folder=""
+for i in [protein, options.machine,machine_description["gromacs"]["version"]]:
+    output_folder+=str(i)+"/"
+    pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
 
-output_folder=protein+"-"+options.id
+# create a list containing the number of cores to consider as a doubling series
+power_of_2=int(math.log(machine_description["cpu"]["cores"])/math.log(2))
+list_number_cores= [ 2**i for i in range(0,power_of_2+1) ]
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# source the relevant GMXRC
-if machine["how-run"]=="bash":
-    OUTPUT=open(output_folder+"/run.sh",'w')
-
-    OUTPUT.write("#! /bin/bash\n")
-    OUTPUT.write(""+"\n")
-
-    line = "source "+ machine["gromacs"]["path"]
-    OUTPUT.write(line+"\n")
-    OUTPUT.write(""+"\n")
-
-
-power_of_2=int(math.log(machine["core-number"])/math.log(2))
-number_cores= [ 2**i for i in range(0,power_of_2+1) ]
-
-if machine["gpu-number"]>0:
-    number_gpus = range(machine["gpu-number-min"],machine["gpu-number"]+1)
+if machine_description["gpu"]["present"]:
+    list_number_gpus = range(machine_description["gpu"]["min"],machine_description["gpu"]["max"])
 else:
-    number_gpus=[0]
+    # if no GPU create a list holding zero
+    list_number_gpus=[0]
 
-# iterate over the number of GPUs
-for ngpu in number_gpus:
+if machine_description["node"]["number"]>1:
+    list_number_nodes=range(1,machine_description["node"]["number"]+1)
+else:
+    list_number_nodes=[1]
 
-    for ntmpi in number_cores:
+for nnode in list_number_nodes:
 
-        power_of_2=int(math.log(machine["core-number"]/ntmpi)/math.log(2))
-        number_threads=[ 2**a for a in range(0,power_of_2+1) ]
+    # iterate over the number of GPUs
+    for ngpu in list_number_gpus:
 
-        for ntomp in number_threads:
+        for ntcore in list_number_cores:
 
-            if (ntmpi*ntomp)<=machine["core-number"]:
+            ntmpi=nnode*ntcore
 
-                stem=protein+'_'+options.id+"_"+machine["gromacs"]["version"]+"_"+str(ntmpi)+"_"+str(ngpu)+"_"+str(ntomp)
+            if machine_description["cpu"]["threads"]:
+                power_of_2=int(math.log(machine_description["cpu"]["cores"]/ntmpi)/math.log(2))
+                list_number_threads=[ 2**a for a in range(0,power_of_2+1) ]
 
-                line=""
+            else:
+                list_number_threads=[1]
 
-                if ngpu>0 and ntmpi==ngpu:
+            for ntomp in list_number_threads:
 
-                    if machine["how-run"]=="scheduler":
+                if (ntmpi*ntomp)<=machine_description["cpu"]["cores"]:
+
+                    stem=protein+'_'+options.machine+"_"+machine_description["gromacs"]["version"]+"_"+str(ntmpi)+"_"+str(ngpu)+"_"+str(ntomp)
+
+                    line=""
+
+                    if ngpu>0 and ntmpi==ngpu:
+                        line=machine_description["gromacs"]["command"]
+
+                    elif ngpu==0:
+
+                        line=machine_description["gromacs"]["command"]
+
+                        # if the machine has GPUs but we aren't using them, add flags to stop GROMACS automatically using any detected GPUs
+                        if machine_description["gpu"]["present"] and machine_description["gpu"]["max"]>0:
+                            line=line.replace("-noconfout","-noconfout -nb cpu")
+
+                    if line!="":
                         OUTPUT=open(output_folder+"/"+stem+".sh","w")
-                        line=machine["gromacs"]["header"]
-                        line+="\n"
-                        line+=machine["gromacs"]["module-cmds"]
-                        line+="\n"
-                        line+=machine["gromacs"]["command"]
-
-                    else:
-                        line=""
-
-                elif ngpu==0:
-
-                    if machine["how-run"]=="scheduler":
-                        OUTPUT=open(output_folder+"/"+stem+".sh","w")
-                        line=machine["gromacs"]["header"]
-                        line+="\n"
-                        line+=machine["gromacs"]["module-cmds"]
-                        line+="\n"
-                        line+=machine["gromacs"]["command"]
-                    else:
-                        line+=machine["gromacs"]["command"]
-
-                    if machine["gpu-number"]>0:
-                        line=line.replace("-noconfout","-noconfout -nb cpu")
-
-                if line!="":
-                    line=line.replace("NTMPI",str(ntmpi))
-                    line=line.replace("NTOMP",str(ntomp))
-                    line=line.replace("TPRFILEPATH",options.protein)
-                    line=line.replace("TPRFILE",str(stem))
-                    line=line.replace("PROTEIN",protein)
-                    OUTPUT.write(line+"\n")
-
-                if machine["how-run"]=="scheduler" and line !="":
-                    OUTPUT.close()
-
-if machine["how-run"]=="bash":
-    OUTPUT.close()
+                        line=line.replace("NTNODES",str(nnode))
+                        line=line.replace("NTMPI",str(ntmpi))
+                        line=line.replace("NTOMP",str(ntomp))
+                        line=line.replace("TPRFILEPATH",options.protein)
+                        line=line.replace("TPRFILE",str(stem))
+                        line=line.replace("PROTEIN",protein)
+                        OUTPUT.write(line+"\n")
+                        OUTPUT.close()
