2a96cf21129330d311d9ab8776d8e9c02f28cccc
Philip Fowler <philipwfowler@gmail.com>
2019-06-26 11:50:39 +0100

    removed purge from archer

--- bin/gromarks-analyse.py
+++ bin/gromarks-analyse.py
@@ -5,56 +5,70 @@ import json
 import math, os
 
 aparser = argparse.ArgumentParser()
-aparser.add_argument("--id",default=None,help="the name of the JSON describing the machine configuration e.g. rescomp.F")
+aparser.add_argument("--machine",default=None,help="the name of the JSON describing the machine configuration e.g. rescomp.F")
 aparser.add_argument("--protein",default=None,help="the name of the benchmarking TPR file e.g. dhfr/rpob/peptst")
 options = aparser.parse_args()
 
 protein=options.protein
 
 resource_package = __name__
-resource_path = '/'.join(('../config/machines/', options.id+".json"))  # Do not use os.path.join()
+resource_path = '/'.join(('../config/machines/', options.machine+".json"))  # Do not use os.path.join()
 machine_file = pkg_resources.resource_filename(resource_package, resource_path)
 
 # load the JSON file
 with open(machine_file,'r') as f:
     machine=json.load(f)
 
-output_folder=options.protein+"-"+options.id
+output_folder=options.protein+"-"+options.machine
 
-power_of_2=int(math.log(machine["core-number"])/math.log(2))
-number_cores= [ 2**i for i in range(0,power_of_2+1) ]
+if 'list' in machine_description["cpu"].keys():
+    list_number_cores=machine_description["cpu"]["list"]
+else:
+    # create a list containing the number of cores to consider as a doubling series
+    power_of_2=int(math.log(machine_description["cpu"]["cores"])/math.log(2))
+    list_number_cores= [ 2**i for i in range(0,power_of_2+1) ]
 
-number_gpus = range(0,machine["gpu-number"]+1)
+if machine_description["gpu"]["present"]:
+    list_number_gpus = range(machine_description["gpu"]["min"],machine_description["gpu"]["max"])
+else:
+    # if no GPU create a list holding zero
+    list_number_gpus=[0]
 
-for ngpu in number_gpus:
-    if ngpu>0:
-        gpu_type="%i x %s" % (ngpu,machine["gpu-type"])
-    else:
-        gpu_type="-"
+if 'list' in machine_description["node"].keys():
+    list_number_nodes=machine_description["node"]["list"]
+else:
+    list_number_nodes=[1]
 
-    for ntmpi in number_cores:
-        power_of_2=int(math.log(machine["core-number"]/ntmpi)/math.log(2))
-        number_threads=[ 2**a for a in range(0,power_of_2+1) ]
+for nnode in list_number_nodes:
 
-        output_line="%15s, %7i, %12s, %10s, %3i, %3i," % (machine['id'],machine['core-number'],gpu_type,machine['gromacs']['version'],ntmpi,ngpu)
-        print_line=False
+    for ngpu in list_number_gpus:
 
-        for ntomp in number_threads:
+        if ngpu>0:
+            gpu_type="%i x %s" % (ngpu,machine["gpu-type"])
+        else:
+            gpu_type="-"
 
-            log_file = options.protein+'_'+options.id+"_"+machine["gromacs"]["version"]+"_"+str(ntmpi)+"_"+str(ngpu)+"_"+str(ntomp)+".log"
+        for ntcore in list_number_cores:
 
-            if os.path.exists(log_file):
+            output_line="%15s, %7i, %12s, %10s, %3i, %3i," % (machine['id'],machine['core-number'],gpu_type,machine['gromacs']['version'],ntnode,ntcore,ngpu)
+            print_line=False
 
-                with open(log_file,'r') as file:
+            for ntomp in list_number_cores:
 
-                    for line in file:
+                log_file =options.protein+'_'+options.machine+"_"+machine_description["gromacs"]["version"]+"_"+str(nnode)+"_"+str(ntcore)+"_"+str(ngpu)+"_"+str(ntomp)
 
-                        if line:
-                            cols=line.split()
+                if os.path.exists(log_file):
 
-                            if cols and cols[0]=="Performance:":
-                                print_line=True
-                                output_line+= " %7.2f, " % (float(cols[1]))
+                    with open(log_file,'r') as file:
 
-        if print_line:
-            print(output_line)
+                        for line in file:
+
+                            if line:
+                                cols=line.split()
+
+                                if cols and cols[0]=="Performance:":
+                                    print_line=True
+                                    output_line+= " %7.2f, " % (float(cols[1]))
+
+            if print_line:
+                print(output_line)
